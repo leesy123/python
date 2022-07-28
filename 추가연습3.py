e2958de684c40b5bc92b5516137ba6275188c934
leesy123 <iiilox20@gmail.com>
2022-07-28 16:14:55 +0900

    추가연습3

---
+++ 추가연습3.py
@@ -0,0 +1,17 @@
+def solution(left, right):
+  ans = 0
+  for i in range(left, right + 1):
+    cnt = 0
+
+    for j in range(1, int(i/2) + 1) :
+      if i // j == i/j :
+        if j ** 2 == i : cnt += 1
+        else : cnt += 2
+    
+    if cnt%2 : ans -= i
+    else : ans += i
+
+  return ans
+
+c = solution(13,17)
+print(c)
