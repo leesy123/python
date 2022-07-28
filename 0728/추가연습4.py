552c6018655e5f15529932c1943c8fa1c1e438c5
leesy123 <iiilox20@gmail.com>
2022-07-28 17:02:53 +0900

    수정

--- 0728/추가연습4.py
+++ 0728/추가연습4.py
@@ -2,7 +2,8 @@ data = int(input('숫자로 이루어진 문자열을 입력하세요. '))
 ans = 1
 
 while data != 0 :
-  if data%10 != 0 : ans *= (data % 10)
+  if data%10 == 1 : ans += data%10
+  elif data%10 != 0 : ans *= (data % 10)
   data = data//10
 
 print(ans)
