17d72885397c66b82f3040dae448cb941c1f9d50
leesy123 <iiilox20@gmail.com>
2022-07-29 16:10:00 +0900

    addQ2

--- 0729/Q2.py
+++ 0729/Q2.py
@@ -1,2 +1,12 @@
 def solution(numbers):
     return 45 - sum(numbers)
+
+def solution1(numbers):
+    answer = 0
+    for i in range(10) :
+        if i not in numbers: answer += i
+    return answer
+
+numbers = [1,2,3,4,6,7,8,0]
+answer = solution1(numbers)
+print(answer)
