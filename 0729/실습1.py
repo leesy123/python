a31e8b46bc249ef3025663c9b1ee0613e38ccb93
leesy123 <iiilox20@gmail.com>
2022-07-29 12:30:26 +0900

    add

--- 0729/실습1.py
+++ 0729/실습1.py
@@ -7,6 +7,21 @@ def primeNum(n) :
         if check==False : a.append(i)
     return a
 
-list = primeNum(1000)
+def Eratos(n) :
+    a = [True] * (n+1)
+    for i in range (2, int((n+1)**0.5) + 1):
+        if a[i] == True :
+            for j in range(i+i, n+1, i):
+                a[j] = False
+
+    return [i for i in range(2,n+1) if a[i] == True]
+            
+n = 1000
+list = primeNum(n)
+list1 = Eratos(n)
 for i in range(len(list)):
     print(f'{list[i]} ', end='')
+print(f' 총 {len(list)}개')
+for i in range(len(list1)):
+    print(f'{list1[i]} ', end='')
+print(f' 총 {len(list1)}개')
