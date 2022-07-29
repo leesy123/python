ac29836a299eb62a5505c1d144eafd1773ec9f25
leesy123 <iiilox20@gmail.com>
2022-07-29 16:05:11 +0900

    수정

--- 0729/Q7.py
+++ 0729/Q7.py
@@ -1,5 +1,6 @@
 def solution(arr):
     answer = []
-    for i in range(len(arr)):
-        if len(answer) == 0 or arr[i] != answer[len(answer)-1] : answer.append(arr[i])
+    answer.append(arr[0])
+    for i in range(1,len(arr)):
+        if arr[i] != answer[len(answer)-1] : answer.append(arr[i])
     return answer
