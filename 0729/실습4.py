103aba842dbb727bf6bc3ff1ccc56f2702148273
leesy123 <iiilox20@gmail.com>
2022-07-29 13:49:38 +0900

    index

--- 0729/실습4.py
+++ 0729/실습4.py
@@ -1,10 +1,16 @@
 num = list(map(int, input().split()))
 minimum = num[0]
 maximum = num[0]
+min_index = 0
+max_index = 0
 
 for i in range(1,len(num)):
-    if minimum > num[i] : minimum = num[i]
-    if maximum < num[i] : maximum = num[i]
+    if minimum > num[i] :
+        minimum = num[i]
+        min_index = i
+    if maximum < num[i] :
+        maximum = num[i]
+        max_index = i
     
-print(f'최대값 : {maximum}')
-print(f'최소값 : {minimum}')
+print(f'최대값 : {maximum} / 인덱스 : {max_index}')
+print(f'최소값 : {minimum} / 인덱스 : {min_index}')
